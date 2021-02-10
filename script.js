518355d351595455be2755d478288be1f8cd6103
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-10 16:40:35 +0000

    stuff changed

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function imagechange() {
-  document.getElementById('myimage').src='./images/dog.jpg';
+  document.getElementById('myimage').src = './images/dog.jpg';
 }
 
 function changeimage() {
-  document.getElementById('myimage').src='./images/cat.jpg';
+  document.getElementById('myimage').src = './images/cat.jpg';
 }
 
 function textchange() {
-  document.getElementById('mytext').innerHTML='Hello World';
+  document.getElementById('mytext').innerHTML = 'Hello World';
 }
 
 function disappear() {
-  document.getElementById('disappear').style.display='none';
+  document.getElementById('disappear').style.display = 'none';
 }
 
 function appear() {
-  document.getElementById('disappear').style.display='block';
+  document.getElementById('disappear').style.display = 'block';
 }
 
 function background() {
-  document.getElementById('background').style.backgroundColor='aqua';
+  document.getElementById('background').style.backgroundColor = 'aqua';
 }
