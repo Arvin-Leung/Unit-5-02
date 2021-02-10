3ca25b56e545c2c2b6b1b3995d7f6ebcb5f317ca
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-10 16:44:01 +0000

    comments

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+//this function changes the Image to a dog
 function imagechange() {
   document.getElementById('myimage').src = './images/dog.jpg';
 }
-
+//this function changes the Image to a cat
 function changeimage() {
   document.getElementById('myimage').src = './images/cat.jpg';
 }
-
+//this function changes the text to Hello World
 function textchange() {
   document.getElementById('mytext').innerHTML = 'Hello World';
 }
-
+//this function makes the text disappear
 function disappear() {
   document.getElementById('disappear').style.display = 'none';
 }
-
+//this function makes the text reappear
 function appear() {
   document.getElementById('disappear').style.display = 'block';
 }
-
+//this this changes the background color
 function background() {
   document.getElementById('background').style.backgroundColor = 'aqua';
 }
