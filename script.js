f7d186fc3a3b5bae825467e4ba37d83c341a11dd
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-10 16:13:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function imagechange() {
+  document.getElementById('myimage').src='./images/dog.jpg';
+}
+
+function changeimage() {
+  document.getElementById('myimage').src='./images/cat.jpg';
+}
+
+function textchange() {
+  document.getElementById('mytext').innerHTML='Hello World';
+}
+
+function disappear() {
+  document.getElementById('disappear').style.display='none';
+}
+
+function appear() {
+  document.getElementById('disappear').style.display='block';
+}
+
+function background() {
+  document.getElementById('background').style.backgroundColor='aqua';
+}
