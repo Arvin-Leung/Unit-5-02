b63123a1e9867b43a34c04e955c877aee5abb59d
ArvinLeung <arvin.leung@ocsbstudent.ca>
2021-02-10 16:47:44 +0000

    space after //

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-//this function changes the Image to a dog
+// this function changes the Image to a dog
 function imagechange() {
   document.getElementById('myimage').src = './images/dog.jpg';
 }
 
-//this function changes the Image to a cat
+// this function changes the Image to a cat
 function changeimage() {
   document.getElementById('myimage').src = './images/cat.jpg';
 }
 
-//this function changes the text to Hello World
+// this function changes the text to Hello World
 function textchange() {
   document.getElementById('mytext').innerHTML = 'Hello World';
 }
 
-//this function makes the text disappear
+// this function makes the text disappear
 function disappear() {
   document.getElementById('disappear').style.display = 'none';
 }
 
-//this function makes the text reappear
+// this function makes the text reappear
 function appear() {
   document.getElementById('disappear').style.display = 'block';
 }
 
-//this this changes the background color
+// this this changes the background color
 function background() {
   document.getElementById('background').style.backgroundColor = 'aqua';
 }
